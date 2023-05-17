ea674532f1d1e184c08defd8c07528d57890882c
Kristjan Kivikangur <Kristjan.Kivikangur@tthk.ee>
2023-05-17 12:22:01 +0300

    Add task view and finish tutorial

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -6,22 +6,45 @@ use App\Models\Task;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Log;
 
 class TasksController extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
     public function index()
     {
-        return "Hello from TasksController index method";
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+
+        return view('tasks', [
+            'tasks' => $tasks
+        ]);
     }
 
 
-    public function create(Task $newTask)
+    public function create(Request $request): RedirectResponse
     {
-        # code...
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/tasks')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/tasks');
     }
-    public function delete(Task $taskToDelete)
+    public function delete($taskToDelete)
     {
-        # code...
+        Task::findOrFail($taskToDelete)->delete();
+        return redirect('/tasks');
     }
 }
---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <!-- Bootstrap Boilerplate... -->
+
+    <div class="panel-body">
+        <!-- Display Validation Errors -->
+        @include('common.errors')
+
+        <!-- New Task Form -->
+        <form action="/tasks" method="POST" class="form-horizontal">
+            {{ csrf_field() }}
+
+            <!-- Task Name -->
+            <div class="form-group">
+                <label for="task" class="col-sm-3 control-label">Task</label>
+
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="task-name" class="form-control">
+                </div>
+            </div>
+
+            <!-- Add Task Button -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> Add Task
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!-- TODO: Current Tasks -->
+    @if (count($tasks)>0)
+        <h1>Current tasks</h1>
+        <table border="1">
+            <th>Task</th><th></th>
+            @foreach ($tasks as $task)
+                <tr>
+                    <td>{{ $task->name }}</td>
+                    <td>
+                        <form action="/tasks/{{ $task->id }}" method="POST">
+                            {{ csrf_field() }}
+                            {{ method_field("DELETE") }}
+                            <button>Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    @endif
+@endsection
